8107925d5b80a08d18a9797ebc9df37949d30d9c
Matheun Struik <matheunstruik@outlook.com>
2024-06-04 20:34:13 +0200

    feat(navbar): added NavBar component to the jobs app

---
+++ apps/jobs/components/NavBar.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const navLinks = ref([
+    {
+        label: "Vacatures",
+        pageName: "index",
+        to: "/",
+    },
+    {
+        label: "Vacature",
+        pageName: "vacature-id",
+        to: "/vacature/P101847",
+    },
+    {
+        label: "Over ons",
+        pageName: "external",
+        to: "https://nobearsgroup.com/nl/over-ons",
+    },
+]);
+
+const nuxtLink = resolveComponent("NuxtLink");
+</script>
+
+<template>
+    <div class="bg-background-primary border-border-secondary border-b">
+        <nav class="container flex items-center justify-between px-8 py-4">
+            <ui-logo />
+            <div>
+                <RadixPrimitive
+                    v-for="(link, index) in navLinks"
+                    :key="index"
+                    :as="nuxtLink"
+                    :to="link.to"
+                >
+                    <ui-button
+                        :label="link.label"
+                        variant="text"
+                        :color="route.name === link.pageName ? 'brand' : 'default'"
+                    />
+                </RadixPrimitive>
+            </div>
+            <!-- {{ route.query }} -->
+            <!-- {{ route. }} -->
+        </nav>
+    </div>
+</template>
